82dea36ce00fb7ed85a9d53589a834425e4a440a
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-04 19:25:35 +0530

    Create matrix.java

---
+++ matrix.java
@@ -0,0 +1,18 @@
+public class Sub_Matrix  
+{  
+    public static void main(String[] args) {  
+        int rows, cols;  
+  
+        //Initialize matrix a  
+          int a[][] = {  
+                          {4, 5, 6},  
+                          {3, 4, 1},  
+                          {1, 2, 3}  
+                       };  
+  
+          //Initialize matrix b  
+          int b[][] = {  
+                          {2, 0, 3},  
+                         {2, 3, 1},  
+                         {1, 1, 1}  
+                     };  

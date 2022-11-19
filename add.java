3cc35210bc8ddc269ce7af20860cc2f9ed9d0a80
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2022-11-19 21:49:58 +0530

    error changed

--- add.java
+++ add.java
@@ -1,7 +1,9 @@
-public class add()
-    public private static void(string[]args){
+public class add{
+    public static void main(String[]args){
         int i=10;
         int j=30;
         int c=i+j;
         System.out.println(c);
     }
+}
+    

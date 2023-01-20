75913154e521f2f81b423bed14037c412fb8134f
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-20 21:49:11 +0530

    Add files via upload

---
+++ initlistusingarrays.java
@@ -0,0 +1,17 @@
+package mypack1;
+import java.util.Arrays;
+import java.util.List;
+
+public class initlistusingarrays {
+
+
+		public static void main(String args[])
+		{
+
+			// Instantiating List using Arrays.asList()
+			List<Integer> list = Arrays.asList(1, 2, 3);
+
+			// Print the list
+			System.out.println("List : " + list.toString());
+		}
+	}

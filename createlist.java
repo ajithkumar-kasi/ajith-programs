953583395f74dea1c5de764d0472bd291f04197e
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-23 21:29:31 +0530

    Add files via upload

---
+++ createlist.java
@@ -0,0 +1,18 @@
+package mypack1;
+import java.util.*;  
+
+public class createlist {
+
+	public static void main(String args[]){  
+	 //Creating a List  
+	 List<String> list=new ArrayList<String>();  
+	 //Adding elements in the List  
+	 list.add("Mango");  
+	 list.add("Apple");  
+	 list.add("Banana");  
+	 list.add("Grapes");  
+	 //Iterating the List element using for-each loop  
+	 for(String fruit:list)  
+	  System.out.println(fruit);  
+	}
+	}  

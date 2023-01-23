5a2250ce4a2cc699383797f95ef3e196e6513e49
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-23 21:30:15 +0530

    Add files via upload

---
+++ cobertarrtolist.java
@@ -0,0 +1,18 @@
+package mypack1;
+import java.util.*;  
+
+public class cobertarrtolist {
+
+	public static void main(String args[]){  
+	//Creating Array  
+	String[] array={"Java","Python","PHP","C++"};  
+	System.out.println("Printing Array: "+Arrays.toString(array));  
+	//Converting Array to List  
+	List<String> list=new ArrayList<String>();  
+	for(String lang:array){  
+	list.add(lang);  
+	}  
+	System.out.println("Printing List: "+list);  
+	  
+	}  
+	}  

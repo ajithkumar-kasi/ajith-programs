fd6cb4e48fcf29e2c8450c3eaaae110052915594
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-23 20:50:27 +0530

    list2 added

--- collectionexa.java
+++ collectionexa.java
@@ -5,7 +5,8 @@ public class collectionexa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		List<Integer> a=new ArrayList<Integer>();
+		ArrayList<Integer> a=new ArrayList<>(20);
+		ArrayList<Integer> a1=new ArrayList<>(List.of(10,20,30,50));
 		a.add(1);
 		a.add(6);
 		a.add(8);
@@ -13,6 +14,9 @@ public class collectionexa {
 		a.add(4);
 		a.add(5);
 		a.add(3);
+		
+		System.out.println(a.contains(1));
+		System.out.println(a);
 		
 		System.out.println(a);
 		System.out.println(a.indexOf(7));

6102c5de337303d6edc87b3327ede79d24896cc5
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-23 20:43:14 +0530

    Add files via upload

---
+++ collectionexa.java
@@ -0,0 +1,24 @@
+package mypack1;
+import java.util.*;
+public class collectionexa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> a=new ArrayList<Integer>();
+		a.add(1);
+		a.add(6);
+		a.add(8);
+		a.add(7);
+		a.add(4);
+		a.add(5);
+		a.add(3);
+		
+		System.out.println(a);
+		System.out.println(a.indexOf(7));
+		System.out.println(a.lastIndexOf(3));
+		
+		
+	}
+
+}

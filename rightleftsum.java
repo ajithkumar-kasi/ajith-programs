c98fa1b320c9367ef40cbc2e004c43e943f1f99b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-01-24 20:27:28 +0530

    Add files via upload

---
+++ rightleftsum.java
@@ -0,0 +1,16 @@
+package mypack1;
+
+public class rightleftsum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {4,1,0,2,3};
+		
+		int mid=a.length/2;
+		System.out.println(mid);
+		System.out.println(a.getClass());
+		
+	}
+
+}

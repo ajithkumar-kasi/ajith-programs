c80ad8972afc30c665159ffe6646fd763f4e6a9d
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-03 21:05:10 +0530

    Add files via upload

--- arraysort.java
+++ arraysort.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class arraysort {
 
@@ -7,8 +7,17 @@ public class arraysort {
 
 		int a[]= {18,32,5,34,6,86};
 		Arrays.sort(a);
+	
+		System.out.println("Ascending order:");
 		for(int x:a) {
-			System.out.println(x);
+			System.out.print(x+",");
+		}
+		System.out.println(" ");
+		System.out.println(Arrays.toString(a));
+		
+		System.out.print("descending order:");
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.print(a[i]+",");
 		}
 		
 	}

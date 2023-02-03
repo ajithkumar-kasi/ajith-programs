3a156bc5156d17fc02a357644725a0f0e97ec853
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-03 20:38:47 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,16 @@
+
+public class increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=9;
+		int b=7;
+		
+		System.out.println(b);
+		b=a++ + a++;
+		
+		System.out.println(a);
+	}
+
+}

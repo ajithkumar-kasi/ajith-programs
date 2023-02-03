6a6a1b7628e01ae982e2aeaebabcfd66777e399f
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-03 20:39:35 +0530

    Add files via upload

---
+++ arraysort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class arraysort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {18,32,5,34,6,86};
+		Arrays.sort(a);
+		for(int x:a) {
+			System.out.println(x);
+		}
+		
+	}
+
+}

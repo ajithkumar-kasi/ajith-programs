23bc8087a895f0a68a5090df5311a00c1b358251
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-04 21:21:57 +0530

    Add files via upload

--- minmaxsum.java
+++ minmaxsum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class minmaxsum {
 	// Java Implementation of the above approach
-		static void minMax(int[] arr)
+	/*	static void minMax(int[] arr)
 		{
 			// Initialize the min_value
 			// and max_value to 0
@@ -34,6 +34,29 @@ public class minmaxsum {
 			int[] arr1 = { 100, 200, 300, 400, 500 };
 			minMax(arr);
 			minMax(arr1);
+		
 		}
+		*/
+
+	    public static void main(String[] args) {
+	      int a[]={1,2,3,4,5,6,7};
+	      int maxsum=0;
+	      int minsum=0;
+	      
+	      for(int i=0;i<a.length-1;i++)
+	      {
+	          minsum +=a[i];
+	      }
+	        System.out.println(minsum);
+	        System.out.println(" ");
+	    
+	      for(int i=a.length-1;i>0;i--)
+	      {
+	          maxsum +=a[i];
+	      }
+	        System.out.println(maxsum);
+	    
+	    }
 	}
+	
 

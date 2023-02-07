85d17b49ad091571b645f0606bd0d782dbad705e
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-07 20:11:43 +0530

    Add files via upload

---
+++ largestin3.java
@@ -0,0 +1,17 @@
+
+public class largestin3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=3,b=5,c=12;
+		
+		int c1=(a>b)?a:b;
+		int c2=(c>c1)?c:c1;
+		
+		System.out.println("the gretest number is:"+c2);
+		
+			
+	}
+
+}

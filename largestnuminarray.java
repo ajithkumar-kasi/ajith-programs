a430a952981dc5bb0a763b4662d7e1402811684d
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-09 20:43:01 +0530

    Add files via upload

---
+++ largestnuminarray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class largestnuminarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {1,34,5,678,3,21,78,23,3};
+		
+		/*Arrays.sort(a);
+		for(int x:a) {
+			System.out.print(x+",");
+		}
+		System.out.println("");
+		System.out.println("The lergest number is:"+a[a.length-1]);
+		System.out.println("The smallest number is:"+a[0]);
+		*/
+		
+	}
+
+}

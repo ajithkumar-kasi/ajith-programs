f11359af3a7ab62fb6ab7e747b4b3f15af2669f9
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-09 21:07:41 +0530

    Add files via upload

--- duplicateelement.java
+++ duplicateelement.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+	
 public class duplicateelement {
 	/*public static int removeduplicates(int a[], int n)
 		{
@@ -26,6 +27,14 @@ public class duplicateelement {
 
 			return j;
 		}*/
+	
+	
+	
+	
+	
+	
+	
+	/*
 		public static void main(String[] args)
 		{
 			int a[] = { 1, 1, 2,4,5,7,8,5,2,2,2 };
@@ -37,7 +46,7 @@ public class duplicateelement {
 			for (int i = 0; i < n; i++)
 				System.out.print(a[i] + " ");
 		
-		*/
+	
 			
 			for(int i=0;i<n;i++) {
 				for(int j=i+1;j<n;j++) {
@@ -47,5 +56,79 @@ public class duplicateelement {
 			}
 		}
 		
-	}
+		*/ 
+		    /* public static void main(String[] args) {      
+		          
+		        //Initialize array   
+		        int [] arr = new int [] {1, 2,2, 3, 4, 2, 7, 8, 8, 3};   
+		          
+		        System.out.println("Duplicate elements in given array: ");  
+		        //Searches for duplicate element  
+		        for(int i = 0; i < arr.length; i++) {  
+		            for(int j = i + 1; j < arr.length; j++) {  
+		                if(arr[i] == arr[j])  
+		                    System.out.println(arr[j]);  
+		            }  
+		        }  
+		    }  */
+	
+	
+	
+	 
+	
+	
+	static void findDuplicates(int arr[], int len)
+	    {
+	 
+	        // initialize ifPresent as false
+	        boolean ifPresent = false;
+	 
+	        
+	        // ArrayList to store the output
+	        ArrayList<Integer> al = new ArrayList<Integer>();
+	 
+	        for (int i = 0; i < len ; i++) {
+	            for (int j = i + 1; j < len; j++) {
+	                if (arr[i] == arr[j]) {
+	                    // checking if element is
+	                    // present in the ArrayList
+	                    // or not if present then break
+	                    if (al.contains(arr[i])) {
+	                        break;
+	                    }
+	 
+	                    // if element is not present in the
+	                    // ArrayList then add it to ArrayList
+	                    // and make ifPresent at true
+	                    else {
+	                        al.add(arr[i]);
+	                        ifPresent = true;
+	                    }
+	                }
+	            }
+	        }
+	 
+	        // if duplicates is present
+	        // then print ArrayList
+	        if (ifPresent == true) {
+	 
+	            System.out.print(al + " ");
+	        }
+	        else {
+	            System.out.print("No duplicates present in arrays");
+	        }
+	    }
+	 
+	    // Driver Code
+	    public static void main(String[] args)
+	    {
+	 
+	        int arr[] = { 12, 11, 40, 12, 5, 6, 5, 12, 11 };
+	        int n = arr.length;
+	 
+	        findDuplicates(arr, n);
+	    }
+	
+		}  
+	
 

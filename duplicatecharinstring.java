21a6547a10ad1599c5a1df4773b0f133c04c5123
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-10 21:00:22 +0530

    Add files via upload

--- duplicatecharinstring.java
+++ duplicatecharinstring.java
@@ -1,11 +1,11 @@
 
 public class duplicatecharinstring {
     public static void main(String[] args) {  
-        String string1 = "Greatresponsibility";  
+        /*String string1 = "Greatresponsibility";  
         int count=0; 
         int n=string1.length();       
         char a[] = string1.toCharArray();  
-        /*  
+         
         System.out.println("Duplicate characters in a given string: ");  
         for(int i = 0; i <string.length; i++) {  
             count = 1;  
@@ -22,16 +22,22 @@ public class duplicatecharinstring {
         } 
         */
         
-        for(int i=0;i<n;i++) {
-        	for(int j=i+1;j<n;j++)
-        	{
-        		if(a[i]==a[j]) {
-        			System.out.print(a[j]);
-        			count++;
-        			break;
-        		}
-        	}
-        }
+
+    	  String str = "w3schoolss are loves";
+    	  int cnt = 0;
+    	  char[] inp = str.toCharArray();
+    	  System.out.println("Duplicate Characters are:");
+    	  for (int i = 0; i < str.length(); i++) {
+    	   for (int j = i + 1; j < str.length(); j++) {
+    	    if (inp[i] == inp[j]) {
+    	     System.out.println(inp[j]);
+    	     cnt++;
+    	     break;
+    	    }
+    	   }
+    	  }
+    	 
+    	
     }  
 
 }

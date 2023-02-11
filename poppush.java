22a3bea69302e678b58b9832fe6b6d15a8923cf9
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-11 20:46:00 +0530

    Add files via upload

--- poppush.java
+++ poppush.java
@@ -4,7 +4,7 @@ public class poppush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Stack<String> stk = new Stack<String>();  
+/*		 Stack<String> stk = new Stack<String>();  
 		stk.push("ajith");
 		stk.push("kumar");
 		stk.push("my");
@@ -13,7 +13,24 @@ public class poppush {
 		
 		stk.pop();
 		System.out.println("the stack elements are: "+stk);
-	}
+	*/
+		     Stack<String> stk = new Stack<String>();  
+		  
+		        /* push() method is used to add elements. */  
+		        stk.push("My");  
+		        stk.push("First");  
+		        stk.push("Stack");  
+		        stk.push("Implementation");  
+		        /* Display the contents of Stack */  
+		        System.out.println("Contents of Stack after addition of elements: " + stk);  
+		        /* Delete elements of stack using pop() method. */  
+		        System.out.println("Popped element 1: " +stk.pop());  
+		        System.out.println("Popped element 2: " +stk.pop());  
+		        /* Display the Stack after deletion. */  
+		        System.out.println("Contents of Stack after deletion of elements: " + stk);  
+		    }  
+		}  
+	
 
-}
+
 

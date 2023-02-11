1cfd1032aeb577661ac6b1075167afa8463735fc
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-11 18:34:56 +0530

    Add files via upload

---
+++ poppush.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class poppush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Stack<String> stk = new Stack<String>();  
+		stk.push("ajith");
+		stk.push("kumar");
+		stk.push("my");
+		stk.push("firwst");
+		stk.push("stack");
+		
+		stk.pop();
+		System.out.println("the stack elements are: "+stk);
+	}
+
+}
+

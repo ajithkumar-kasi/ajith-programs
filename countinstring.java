5b8d6b06f5c1eedc1e8de47ba2c93590b1d35c54
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-12 22:12:22 +0530

    Add files via upload

---
+++ countinstring.java
@@ -0,0 +1,16 @@
+
+public class countinstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a="I am ajith kumar";
+		int count=0;
+		char[] s=a.toCharArray();
+		for(int i=0;i<s.length;i++) {
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}

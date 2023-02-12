5c65066dddd5a154288648f99c94b47c9b9f6c7e
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-12 22:31:29 +0530

    Add files via upload

--- reversestringrecursion.java
+++ reversestringrecursion.java
@@ -3,7 +3,6 @@ public class reversestringrecursion {
 
 	public String reverseString(String str)  
 	{  
-	//checks if the string is empty   
 	if(str.isEmpty())  
 	{  
 	System.out.println("String is empty.");  
@@ -11,10 +10,14 @@ public class reversestringrecursion {
 	return str;  
 	}   
 	else   
-	{  
-	return reverseString(str.substring(1))+str.charAt(0);  
+	{ 
+		
+	//return reverseString(str.substring(1))+str.charAt(0);  
+	return reverseString(str.substring(0,str.length()-1));
 	}  
-	}  
+	} 
+		
+		
 	public static void main(String[] args)   
 	{  
 	reversestringrecursion rs = new reversestringrecursion();  

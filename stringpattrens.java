83f761cc352b65c7f9d65123e88d6722c4e71b3d
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-15 20:57:35 +0530

    Add files via upload

--- stringpattrens.java
+++ stringpattrens.java
@@ -10,7 +10,7 @@ public class stringpattrens {
 			System.out.println("");
 		}
 		System.out.println("");
-
+//1
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5-i+1;j++) {
 				System.out.print("*");
@@ -18,7 +18,7 @@ public class stringpattrens {
 			System.out.println("");
 		}
 		System.out.println("");
-
+//2
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5-i+1;j++) {
 				System.out.print(j);
@@ -26,11 +26,50 @@ public class stringpattrens {
 			System.out.println("");
 		}
 		System.out.println("");
-
+//3
 		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print("*");
+			for(int j=1;j<=5;j++) {
+				if(j+i>5){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
 			}
+			System.out.println("");
+		}
+		System.out.println("");
+//4		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j>=i){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+//5
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j>=i){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			for(int j=5;j<=1;j--) {
+					System.out.print("*");
+				
+				
+			}
+			
 			System.out.println("");
 		}
 		System.out.println("");

c1b241f852e287da50080f7047d130d5cd087e55
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-15 21:02:02 +0530

    Add files via upload

--- stringpattrens.java
+++ stringpattrens.java
@@ -64,11 +64,41 @@ public class stringpattrens {
 				}
 				
 			}
-			for(int j=5;j<=1;j--) {
+			for(int j=1;j<=5;j++) {
+				if(j<=5-i+1) {
 					System.out.print("*");
-				
+				}
+				else {
+					System.out.print(" ");
+				}
 				
 			}
+			
+			
+			System.out.println("");
+		}
+		System.out.println("");
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j+i>5){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			for(int j=2;j<=5;j++) {
+				if(j<=i) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+			}
+			
 			
 			System.out.println("");
 		}

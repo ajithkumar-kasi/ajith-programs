54822ae155786c026768602e19c3ec715cf47fc2
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-15 20:14:03 +0530

    Add files via upload

---
+++ stringpattrens.java
@@ -0,0 +1,40 @@
+
+public class stringpattrens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i+1;j++) {
+				System.out.print(j);
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+
+	}
+
+}

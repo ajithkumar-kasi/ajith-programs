298f2e4669f119a893674b37ae71888874540c90
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-16 13:42:26 +0530

    Add files via upload

--- swapelementsarr.java
+++ swapelementsarr.java
@@ -5,7 +5,7 @@ public class swapelementsarr {
 		// TODO Auto-generated method stub
 
 		int a[]= {1,2,3,4,5};
-		
+//left		
 		for(int j=0;j<3;j++) {
 			int n=a[0];
 			for(int i=0;i<a.length-1;i++) {
@@ -17,8 +17,26 @@ public class swapelementsarr {
 		
 		
 		for(int i=0;i<a.length;i++) {
-			System.out.println(a[i]);
+			System.out.print(a[i]);
 		}
+		
+		System.out.println("");
+		
+	//right
+		int b[]= {1,2,3,4,5};
+			int n=b[b.length-1];
+			for(int i=b.length-1;i>0;i--) {
+				b[i]=b[i-1];
+				
+				}
+			b[0]=n;
+		
+		
+		
+		for(int i=0;i<b.length;i++) {
+			System.out.print(b[i]);
+		}
+		
 			
 		}
 		

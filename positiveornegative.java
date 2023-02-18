49cfa010b63656ff85eb08df8d3e62d211e840fe
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-18 23:01:26 +0530

    Add files via upload

---
+++ positiveornegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class positiveornegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		
+		if(n>=1) {
+			System.out.println("the given number is positive");
+		
+		}
+			
+			else {
+			System.out.println("the given number is negative");
+
+	}}
+
+}

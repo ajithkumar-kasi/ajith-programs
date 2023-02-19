570b0fe91aa458251bcb620d5bae01387bde2c30
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-19 13:47:48 +0530

    Add files via upload

--- deleteelementinarr.java
+++ deleteelementinarr.java
@@ -1,22 +1,40 @@
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class deleteelementinarr {
 
     public static void main(String[] args) {
+    	Scanner s=new Scanner(System.in);
+    	int n=s.nextInt();
         int a[]={1,2,3,4,7,9,6,87};
-        int n=4;
+      
         int b[]=new int[a.length-1];
         
         
-        for(int i=0,j=0;i<a.length;i++){
+        /* for(int i=0,j=0;i<a.length;i++){
             if(a[i]!=n){
             	b[j++]=a[i];
             }
       
+        }*/
+        for(int i=0,j=0;i<a.length;i++) {
+        	
+        		if(a[i]!=n) {
+        			b[j++]=a[i];
+        		}
+        		
         }
-        for(int x:b)
+      
+
+            System.out.print(Arrays.toString(b));
+         
+        /*
+        for(int i=n;i<a.length-1;i++) {
+        	a[i]=a[i+1];
+        	
+        }
+        for(int x:a)
 
             System.out.print(x+",");
-         
-    }
+   */ }
 }
 

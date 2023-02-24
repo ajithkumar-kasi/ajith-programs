8a838a035a1117caf5e6fa2ede80de6133b9b220
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-24 08:49:51 +0530

    Add files via upload

---
+++ newll.java
@@ -0,0 +1,25 @@
+package mypack1;
+import java.util.LinkedList;
+
+
+public class newll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<Integer> l=new LinkedList<Integer>();
+		l.add(3);
+		l.add(43);
+		l.add(12);
+		l.add(32);
+		l.add(33);
+		l.add(5);
+		
+		l.add(4);
+		System.out.println(l);
+		System.out.println("");
+		l.pop();
+		System.out.println(l);
+	}
+
+}

b0b79db010aac7fd0bcace7165969c3606a6c77b
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-25 20:45:34 +0530

    Add files via upload

--- nonrepeatingcharinstring.java
+++ nonrepeatingcharinstring.java
@@ -4,21 +4,18 @@ public class nonrepeatingcharinstring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean flag=true;
+	
 		String s="hashmaps";
 		char[]c=s.toCharArray();
 		for(int i=0;i<c.length;i++) {
 			for(int j=i+1;j<c.length;j++) {
-				if(c[i]==c[j]) 
-					System.out.print(c[j]);
-					flag=false;
-					break;
+				if(c[i]==c[j]&&c[i]!=' ') {
+					System.out.println(c[i]);
+				}
 			}
-			break;
+			
 		}
-		if(flag) {
-			System.out.print("there is no non repeating character");
-		}
+		
 	}
 
 }

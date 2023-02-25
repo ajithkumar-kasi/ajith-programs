6e76412a57b8f14cfe1ce49e753b419558dcfde0
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-02-25 20:40:41 +0530

    Add files via upload

--- countinstring.java
+++ countinstring.java
@@ -1,27 +1,20 @@
+package mypack1;
 
 public class countinstring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String a="I am ajith kumar";
 		int count=0;
-		char[] s=a.toCharArray();
-		/*for(int i=0;i<s.length;i++) {
+		String s="ajithkumar ";
+		char[]c=s.toCharArray();
+		for(int i=0;i<c.length;i++) {
+			if(c[i]!=' ')
 			count++;
+			
 		}
 		System.out.println(count);
-	*/
-		for(int i=0;i<s.length;i++) {
-			
-			if(s[i]=='a')
-			count++;
-		}
-		System.out.println(" 'a' arrived  is arrvied "+count+" time ");
-	
-		for(int i=s.length-1;i>=0;i--) {
-			System.out.print(s[i]);
-		}
+		
 	}
 
 }

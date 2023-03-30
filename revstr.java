9cdb5aa9bd5f2408c62023e180a82ba6051dc3d3
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-03-30 23:48:37 +0530

    Add files via upload

--- revstr.java
+++ revstr.java
@@ -10,13 +10,16 @@ public class revstr {
 		for(int i=a.length-1;i>=0;i--) {
 			System.out.print(a[i]);
 		}*/
-		String d="";
+		/*String d="";
 		for(int i=0;i<=s.length()-1;i++) {
 				d=s.charAt(i)+d;
 		}
 		System.out.print(d);
-
-		
+*/
+		String[] d=s.split(" ");
+		for(int i=d.length-1;i>=0;i--) {
+			System.out.print(d[i]+" ");
+		}
 	}
 
 }

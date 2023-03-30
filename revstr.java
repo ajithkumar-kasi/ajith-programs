97bea5d007ac19db2d3ec7d3be3d3a5cbd909121
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-03-30 23:46:02 +0530

    Add files via upload

--- revstr.java
+++ revstr.java
@@ -5,10 +5,18 @@ public class revstr {
 		// TODO Auto-generated method stub
 
 		String s="ajithkumar is typing...";
+		/*
 		char[] a=s.toCharArray();
 		for(int i=a.length-1;i>=0;i--) {
 			System.out.print(a[i]);
+		}*/
+		String d="";
+		for(int i=0;i<=s.length()-1;i++) {
+				d=s.charAt(i)+d;
 		}
+		System.out.print(d);
+
+		
 	}
 
 }

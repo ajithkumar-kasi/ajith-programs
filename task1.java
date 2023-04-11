e468ecd813f882d5623aa2e376f9e42e205167fc
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2023-04-11 10:49:52 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,17 @@
+
+public class task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=5;
+		boolean flag=true;
+		if(x=5||flag==true) {
+			System.out.println("hello");
+		}
+		else {
+			System.out.println("world");
+		}
+	}
+
+}
